f884a7cc34b929359d37ab5622086824eebd520b
Stephan Smith <stephan@meenta.io>
2019-10-09 19:41:05 -0400

    Updated the body-parser

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const _ = require('underscore')
+var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 9001
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // Check to see if we have the settings we need.
 var settings = [ 'TAKESHAPE_PROJECTID', 'TAKESHAPE_KEY', 'ALGOLIA_APPID', 'ALGOLIA_ADMIN_KEY' ]
