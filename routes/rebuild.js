613b5e59083d96f2e7dd6f313341d9e19a508b51
Stephan Smith <stephan@meenta.io>
2019-10-09 19:23:28 -0400

    Updated the endpoints

--- routes/rebuild.js
+++ routes/rebuild.js
@@ -1,14 +1,9 @@
 const fetch = require('node-fetch').default;
 const _ = require('underscore')
-const algoliasearch = require('algoliasearch');
+const algolia = require('./services/algolia');
+const takeshape = require('./services/takeshape')
 
-// Config variables.
-const TAKESHAPE_PROJECTID = process.env.TAKESHAPE_PROJECTID
-const TAKESHAPE_KEY = process.env.TAKESHAPE_KEY
-const ALGOLIA_APPID = process.env.ALGOLIA_APPID
-const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY
-
-const client = algoliasearch(ALGOLIA_APPID, ALGOLIA_ADMIN_KEY);
+// Setup the index.
 const index = client.initIndex('cheese');
 
 var query = `{
@@ -32,46 +27,38 @@ var query = `{
 	}
 `
 
-exports.handler = function(event, context, callback) {
+module.export = (req, res) => {
 
-	fetch(`https://api.takeshape.io/project/${TAKESHAPE_PROJECTID}/graphql`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${TAKESHAPE_KEY}`
-		},
-		body: JSON.stringify({ query })
-	}).then(res => {
-		return res.json();
-	}).then(res => {
-			var items = res.data.getCheeseList.items
-			var list = [];
+	takeshape(query).then(res => {
 
-			_.each(items, item => {
-				var object = item
-				item.objectID = item._id;
-				if (item.photo) item.photoUrl = 'https://images.takeshape.io/' + item.photo.path;
-				list.push(item)
-			})
+		var items = res.data.getCheeseList.items
+		var list = [];
 
-			index
+		// Loop the fields and set the data as needed.
+		_.each(items, item => {
+			var object = item
+			item.objectID = item._id;
+			if (item.photo) item.photoUrl = 'https://images.takeshape.io/' + item.photo.path;
+			list.push(item)
+		})
+
+		index
 			.addObjects(list)
 			.then((data) => {
 				console.log('Indexed', data);
 				var count = data.objectIDs.length;
 
-				callback(null, {
-					statusCode: 200,
-					body: `Rebuild all takeshape data to the index ${count}`
-				});
+				res.status(200).send({ message: `Rebuild all takeshape data to the index ${count}` })
 			})
 			.catch(err => {
 				console.log('Got an Eror', err);
-				callback(err, {
-					statusCode: 500,
-					body: err.message
-				});
+				res.status(500).send({ message: err.message })
 			});
-	});
+
+	}).catch(err => {
+		res.status(500).send({ message: err.message })
+	})
+
+
 
 }
