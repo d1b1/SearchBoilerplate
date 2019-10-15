3df3261feb11f1c1bf88e70f958caa7027ddaf82
Stephan Smith <stephan@meenta.io>
2019-10-14 21:08:08 -0400

    Updated again

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: node webhooks/server.js
--- services/algolia.js
+++
@@ -1,5 +0,0 @@
-const algoliasearch = require('algoliasearch')
-const ALGOLIA_APPID = process.env.ALGOLIA_APPID
-const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY
-
-module.exports = algoliasearch(ALGOLIA_APPID, ALGOLIA_ADMIN_KEY);;
